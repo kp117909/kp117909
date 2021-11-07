fe244242543e82e6ea54866e5f835cc4eabd9527
Kamil <kpolak491@gmail.com>
2021-11-07 21:37:01 +0100

    ASD

--- src/AISD/DzieliZwyziecaj_correct.java
+++ src/AISD/DzieliZwyziecaj_correct.java
@@ -9,11 +9,11 @@ public class DzieliZwyziecaj_correct {
             return ilosc_prob;
         }
         if(szukana < srodek){
-            System.out.println("Przedział ["+srodek+","+ min +"] Liczba jest za duża");
+            System.out.println("Przedział ["+min+","+ srodek +"]Liczba "+  srodek + " jest za duża");
             return znajdz(min, srodek -1, szukana , ilosc_prob +1);
         }
         else{
-            System.out.println("Przedział ["+srodek+","+max+"]Liczba jest za mała");
+            System.out.println("Przedział ["+srodek+","+max+"]Liczba " +srodek + " jest za mała");
             return znajdz(srodek + 1, max, szukana ,ilosc_prob +1);
         }
     }
@@ -21,7 +21,7 @@ public class DzieliZwyziecaj_correct {
     public static void main(String[] args) {
         int min = 0;
         int max = 1000;
-        int szukana = 2;
-        System.out.println("Liczba " + szukana + " znaleziona w " + znajdz(min,max,szukana,1) + " podejściu");
+        int szukana = 710;
+        System.out.println("Liczba " + szukana + " znaleziona w " + znajdz(min,max,szukana,1) + " podejściach");
     }
 }
